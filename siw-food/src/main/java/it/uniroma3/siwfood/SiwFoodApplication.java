7037e40c06105eb6a34e62ae7ce181a61bac754e
allozziotharo <01fgermano@gmail.com>
2024-05-06 16:40:39 +0200

    commit di prova

---
+++ siw-food/src/main/java/it/uniroma3/siwfood/SiwFoodApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFoodApplication.class, args);
+	}
+
+}
---
+++ siw-food/src/test/java/it/uniroma3/siwfood/SiwFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
